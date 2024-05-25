08866b1b65f677be2bc59c1c0b66cda79187807c
MatthiasLJK <jklee.2022@scis.smu.edu.sg>
2024-05-25 14:31:35 +0800

    navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,38 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import PWABadge from "./components/PWABadge.vue";
-
+import TabMenu from 'primevue/tabmenu';
 import Button from "primevue/button";
+
+const items= [
+                {
+                   label:'Home',
+                   icon:'',
+                   route:'/',
+
+                },
+                {
+                   label:'Opportunities',
+                   icon:'',
+                   route:'',
+                  },
+
+                {
+                   label:'Groups',
+                   icon:'',
+                   route:'',
+                  },
+                {
+                   label:'My Profile',
+                   icon:'',
+                   route:'',
+                },
+                {
+                   label:'About',
+                   icon:'',
+                   route:'/About',
+                }
+             ]
 </script>
 
 <template>
@@ -25,8 +55,23 @@ import Button from "primevue/button";
 
   <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
   <nav>
+    <TabMenu :model="items">
+      <template #item="{ item, props }">
+          <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+              <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                  <span v-bind="props.icon" />
+                  <span v-bind="props.label">{{ item.label }}</span>
+              </a>
+          </router-link>
+          <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+              <span v-bind="props.icon" />
+              <span v-bind="props.label">{{ item.label }}</span>
+          </a>
+      </template>
+    </TabMenu>
+<!-- 
     <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink> -->
   </nav>
   <main>
     <RouterView />
