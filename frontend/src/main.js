9e5cf03a1d8fd84e9efe9f8ec8876569513b151d
YAP SAI WEI MARCUS <yap.marcus@yahoo.com.sg>
2024-05-25 14:13:07 +0800

    pages skeleton

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -19,21 +19,42 @@ import "primevue/resources/themes/aura-light-cyan/theme.css";
 import { createMemoryHistory, createRouter } from "vue-router";
 import HomeView from "./views/HomeView.vue";
 import AboutView from "./views/AboutView.vue";
-const routes = [
-  { path: "/", name: "home", component: HomeView },
-  //   { path: "/:username", name: "home", component: HomeView },
-  { path: "/about", name: "about", component: AboutView },
-];
-const router = createRouter({
-  history: createMemoryHistory(),
-  routes,
-});
-router.beforeEach((to, from) => {
-  const piniaStore = useMyStore(pinia);
-  console.log("Router beforeEach: ", piniaStore.count);
+import SearchView from "./views/SearchView.vue";
+import GroupCreationView from "./views/GroupCreationView.vue";
+import GroupDashboardView from "./views/GroupDashboardView.vue";
 
-  // return true to continue the navigation
-  return true;
+const routes = [
+	{ path: "/", name: "home", component: HomeView },
+	//   { path: "/:username", name: "home", component: HomeView },
+	{ path: "/about", name: "about", component: AboutView },
+	{
+		path: "/search",
+		name: "search",
+		component: SearchView,
+	},
+	{
+		path: "/groupcreation",
+		name: "groupcreation",
+		component: GroupCreationView,
+	},
+	{
+		path: "/groupdashboard",
+		name: "groupdashboard",
+		component: GroupDashboardView,
+	},
+];
+
+const router = createRouter({
+	history: createMemoryHistory(),
+	routes,
+});
+
+router.beforeEach((to, from) => {
+	const piniaStore = useMyStore(pinia);
+	console.log("Router beforeEach: ", piniaStore.count);
+
+	// return true to continue the navigation
+	return true;
 });
 
 const app = createApp(App);
@@ -42,7 +63,8 @@ app.use(pinia);
 app.use(PrimeVue);
 
 app.use(vue3GoogleLogin, {
-  clientId: "833343365269-kkr166976fa9pnm0npkub9m32o2shbgf.apps.googleusercontent.com",
+	clientId:
+		"833343365269-kkr166976fa9pnm0npkub9m32o2shbgf.apps.googleusercontent.com",
 });
 
 app.mount("#app");
---
+++ frontend/src/views/GroupCreationView.vue
@@ -0,0 +1,15 @@
+<script setup>
+	import Button from "primevue/button";
+	import axios from "axios";
+	import { useMyStore } from "@/stores/mystore.js";
+
+	const myStore = useMyStore();
+</script>
+
+<template>
+	<div>
+		<h1>Group Creation</h1>
+	</div>
+</template>
+
+<style scoped></style>
